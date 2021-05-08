51bb7bb4acfcd44859a224246ab963f23d289f1a
Angel Francis <angelfrancis1111@gmail.com>
2021-05-08 14:11:39 +0530

    new html

---
+++ index2.js
@@ -0,0 +1,26 @@
+function imageEnter(event) {
+	// when mouse move on selector
+	$("#section1 #pic-img").css(
+		"background-image",
+		"radial-gradient(transparent,transparent,rgba(255,255,255,0.2))"
+	);
+	x = event.pageX / 20;
+	y = event.pageY / 20;
+	x = x / 28;
+	y = y / 28;
+	if (x < 0.5) x = -1 * (x - 0.5);
+	else x = 0.5 - x;
+	if (y < 0.5) y = -1 * (y - 0.5);
+	else y = 0.5 - y;
+	$("#section1 #pic-img").css(
+		"transform",
+		"rotateX(" + 50 * x + "deg) rotateY(" + 50 * y + "deg) scale(1.12)"
+	);
+}
+function imageLeave(event) {
+	$("#section1 #pic-img").css("background-image", "");
+	$("#section1 #pic-img").css(
+		"transform",
+		"rotateX(0deg) rotateY(0deg) scale(1)"
+	);
+}
