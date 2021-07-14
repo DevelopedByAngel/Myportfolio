0bbfdee05afbf56743fb95fc3f04d9d0598fb80d
Angel Francis <angelfrancis1111@gmail.com>
2021-07-14 20:08:12 +0530

    Auto-committing 'index2.js'

--- index2.js
+++ index2.js
@@ -24,34 +24,8 @@ function imageLeave(event) {
 		"rotateX(0deg) rotateY(0deg) scale(1)"
 	);
 }
+var angle=0
 $('.pre').on('click',()=>{
-	// var matrix=$('.carousel').css('transform');
-	// const values = matrix.split('(')[1].split(')')[0].split(',');
-	// console.log(values)
- //    const a = values[0];
- //    const b = values[1];
- //    var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
- //    console.log(angle)
-	$('.carousel').css('transform','rotateY('+50+'deg)');
-	// console.log($('.carousel').css('transform'))
-	var carousel=document.getElementsByClassName('carousel')[0];
-	var st = window.getComputedStyle(carousel, null);
-	var tr = st.getPropertyValue("transform")
-	var values = tr.split('(')[1];
-      values = values.split(')')[0];
-      values = values.split(',');
-    var a = values[0];
-    var b = values[1];
-    var c = values[2];
-    var d = values[3];
-
-    var scale = Math.sqrt(a*a + b*b);
-
-    // arc sin, convert from radians to degrees, round
-    /** /
-    var sin = b/scale;
-    var angle = Math.round(Math.asin(sin) * (180/Math.PI));
-    /*/
-    var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
-	console.log(st.getPropertyValue("transform") ,angle);
+	$('.carousel').css('transform','rotateY'+angle+'deg');
+	angle=angle+10;
 })
