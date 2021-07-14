8e5dba2214ca51b688d43b3472feb1cec78cb556
Angel Francis <angelfrancis1111@gmail.com>
2021-07-14 19:40:48 +0530

    Auto-committing 'index2.js'

--- index2.js
+++ index2.js
@@ -25,6 +25,6 @@ function imageLeave(event) {
 	);
 }
 $('.pre').on('click',()=>{
-	console.log($('.carousel').css('transform')[0])
+	console.log($('.carousel').css('transform').rotateY)
 	$('.carousel').css('transform','rotateY('+10+'deg)')
 })
