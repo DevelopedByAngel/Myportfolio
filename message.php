9a8911aea4df94f16c79b89e84bd4f12372cfec6
Angel Francis <angelfrancis1111@gmail.com>
2021-09-29 16:06:51 +0530

    Auto-committing 'message.php'

--- message.php
+++ message.php
@@ -2,8 +2,7 @@
 $to_email = 'angelfrancis1111@gmail.com';
 $subject = 'Testing PHP Mail';
 $message = 'This mail is sent using the PHP mail function';
-$headers = 'From: noreply @ company . com';
-mail($to_email,$subject,$message,$headers);
+mail($to_email,$subject,$message);
 ?>
 <?php
 echo "hello";
