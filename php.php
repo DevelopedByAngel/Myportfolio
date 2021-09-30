efe58335169a2730de1648080b8accffff326933
Angel Francis <angelfrancis1111@gmail.com>
2021-09-30 10:17:55 +0530

    Auto-committing 'php.php'

--- php.php
+++ php.php
@@ -1,7 +1,7 @@
 <?php
 require 'php/includes/PHPMailer.php'; 
 require 'php/includes/SMTP.php';
-require 'php/'
+require 'php/inclu'
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
