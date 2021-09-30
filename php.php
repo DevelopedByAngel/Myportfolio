93ee2d197172050bc886695668b97f36c18ae471
Angel Francis <angelfrancis1111@gmail.com>
2021-09-30 10:31:07 +0530

    Auto-committing 'php.php'

--- php.php
+++ php.php
@@ -1,4 +1,9 @@
 <?php
+if (isset($_POST['submitmessage'])) 
+{
+    $message = $_POST['message'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
 require 'php/includes/PHPMailer.php'; 
 require 'php/includes/SMTP.php';
 require 'php/includes/Exception.php';
@@ -16,11 +21,12 @@ $mail->Username="remainderevent@gmail.com";
 $mail->Password="remainder111";
 $mail->Subject="hello";
 $mail->setFrom("remainderevent@gmail.com");
-$mail->Body="message";
+$mail->Body="message"+$message;
 $mail->addAddress("angelfrancis1806@gmail.com");
 if($mail->Send())
 echo "sent";
 else
 echo "error";
 $mail->smtpClose();
+}
 ?>
