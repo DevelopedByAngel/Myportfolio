0d759a11825ec8588ebd4f23b38c1b7401349b1b
Angel Francis <angelfrancis1111@gmail.com>
2021-10-12 15:09:21 +0530

    Auto-committing 'index2.js'

--- index2.js
+++ index2.js
@@ -77,7 +77,7 @@ const submit=(name,mail,contact,message)=>
 		true
 	); //getting the last status
 	var reply="From: "+name+"("+mail+")\nContact.No:"+contact+"\n\n "+message;
-	var subject="Angel Francis"
+	var subject="Angel Francis | P"
 	var body="<div><div style='background: #ddfff9; border: 7px double white; border-radius: 1rem; padding: 10px;'><span><h1 style='color:#05ab8e;'>Hi "+name+"!!</h1></span><span><h3 style='color: #066f5d;'>Happy to connect with you.</h3></span><br><br><br><h4><span>From,</span><div style='padding:1rem 2rem;line-height:1.5rem;'>Angel F ,<br>Final Year Under Graduate,<br>Panimalar Engineering College.</div></h4></div></div>"
 	xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
 	xmlHttp.send("name="+name+"&email="+mail+"&mobile="+contact+"&message="+message+"&body="+body+"&reply="+reply"&sendmail=true");
