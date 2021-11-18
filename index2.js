581d6153efa90eea42a69cf9936718a72f4decb8
Angel Francis <angelfrancis1111@gmail.com>
2021-11-18 08:05:11 +0530

    Auto-committing 'index2.js'

--- index2.js
+++ index2.js
@@ -81,7 +81,9 @@ const submit=async(name,mail,contact,message)=>
 	var body="<div><div style='background: #ddfff9; border: 7px double white; border-radius: 1rem; padding: 10px;'><span><h1 style='color:#05ab8e;'>Hi "+name+"!!</h1></span><span><h3 style='color: #066f5d;'>Happy to connect with you.</h3></span><br><br><br><h4><span>From,</span><div style='padding:1rem 2rem;line-height:1.5rem;'>Angel F ,<br>Final Year Under Graduate,<br>Panimalar Engineering College.</div></h4></div></div>"
 	await xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
 	await xmlHttp.send("name="+name+"&email="+mail+"&mobile="+contact+"&message="+message+"&body="+body+"&reply="+reply+"&subject="+subject+"&sendmail=true");
-	xmlHttp.onload();
+	xmlHttp.onload=()=>{
+		
+	};
 	equip = xmlHttp.responseText;
 	console.log(equip);
 	console.log(xmlHttp.status);
