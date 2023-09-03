a1116d8631b2ddb09963bf137d618e3b65ea6da3
developedbyangel <angelfrancis1111@gmail.com>
2023-09-03 18:21:24 +0530

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ const mailTo=(mail,subject,body,callback)=>{
         Username: "ang311806@gmail.com",
         Password: "76CA546695B9792DDC89D31EF1DB1132E26D",
         // SecureToken:"994268e4-5260-4d15-abe5-004c6ceb37c1",
-        To: 'angelfrancis1111@gmail.com',
+        To: mail,
         From: "ang311806@gmail.com",
         Subject: "Thanks for getting in touch",
         Body: "Hi <br>ok",
