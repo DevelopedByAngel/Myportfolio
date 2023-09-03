29339f917e6f6db1b55b96e048a4bb226a8cbab7
developedbyangel <angelfrancis1111@gmail.com>
2023-09-03 17:54:15 +0530

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -78,8 +78,8 @@ console.log(Email)
 const mailO=()=>{
       Email.send({
         Host: "smtp.elasticemail.com",
-        Username: "angelfrancis@portfolio.com",
-        Password: "99408AC8D120E93DA236F0152D4565424D68",
+        Username: "ang311806@gmail.com",
+        Password: "76CA546695B9792DDC89D31EF1DB1132E26D",
         // SecureToken:"994268e4-5260-4d15-abe5-004c6ceb37c1",
         To: 'ang311806@gmail.com',
         From: "ang311806@gmail.com",
