c7f85843c142445bcaa4f118f261cb724b6905fb
developedbyangel <angelfrancis1111@gmail.com>
2023-09-03 18:06:34 +0530

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ const mailO=()=>{
         Username: "ang311806@gmail.com",
         Password: "76CA546695B9792DDC89D31EF1DB1132E26D",
         // SecureToken:"994268e4-5260-4d15-abe5-004c6ceb37c1",
-        To: 'angelfrancis1111@gmail.com',
+        To: 'ang311806@gmail.com',
         From: "ang311806@gmail.com",
         Subject: "Sending Email using javascript",
         Body: "Well that was easy!!",
