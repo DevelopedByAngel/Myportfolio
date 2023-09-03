ec18b3ac68a6f2849e199b19864963bb9a085557
developedbyangel <angelfrancis1111@gmail.com>
2023-09-03 18:17:14 +0530

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ const mailO=()=>{
         To: 'angelfrancis1111@gmail.com',
         From: "ang311806@gmail.com",
         Subject: "Thanks for getting in touch",
-        Body: "Hi name\r\nRegards",
+        Body: "Hi <br>ok",
       })
         .then(function (message) {
         	console.log(message)
