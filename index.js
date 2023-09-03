e68dc81fbbe86ae5f26c9ed965b0a7e7d456f59a
developedbyangel <angelfrancis1111@gmail.com>
2023-09-03 18:06:24 +0530

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -80,17 +80,18 @@ const mailO=()=>{
         Host: "smtp.elasticemail.com",
         Username: "ang311806@gmail.com",
         Password: "76CA546695B9792DDC89D31EF1DB1132E26D",
+        // SecureToken:"994268e4-5260-4d15-abe5-004c6ceb37c1",
         To: 'angelfrancis1111@gmail.com',
         From: "ang311806@gmail.com",
-        Subject: "Thanks for getting in touch",
-        Body: "Hi name,Happy to connect with you.Regards,Angel F",
+        Subject: "Sending Email using javascript",
+        Body: "Well that was easy!!",
       })
         .then(function (message) {
         	console.log(message)
           alert("mail sent successfully")
         });
 }
-mailO();
+	mailO();
 
 const submit=async(name,mail,contact,message)=>
 {
@@ -106,7 +107,6 @@ const submit=async(name,mail,contact,message)=>
  //    })
  //      .then((res) => res.json())
  //      .then((r)=>console.log(r));
-	
 	var xmlHttp = new XMLHttpRequest();
 	xmlHttp.open(
 		"POST",
