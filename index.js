5a25ae3c97a5df38d27570d5dee0ebc2d10440f7
developedbyangel <angelfrancis1111@gmail.com>
2023-09-03 11:37:15 +0530

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -116,5 +116,18 @@ if(xmlHttp.status == 200)
 }
 
 const mail=()=>{
-	
+	function sendEmail() {
+      Email.send({
+        Host: "smtp.gmail.com",
+        Username: "sender@email_address.com",
+        Password: "Enter your password",
+        To: 'receiver@email_address.com',
+        From: "sender@email_address.com",
+        Subject: "Sending Email using javascript",
+        Body: "Well that was easy!!",
+      })
+        .then(function (message) {
+          alert("mail sent successfully")
+        });
+    }
 }
