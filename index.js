5a6a053a5819335e12de095c18898cd6ad48c259
developedbyangel <angelfrancis1111@gmail.com>
2023-09-03 18:20:58 +0530

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ $("#contactform").on("submit",(e)=>
 		submit(name,mail,contact,message);
 	});
 console.log(Email)
-const mailTo=(mail,subjec)=>{
+const mailTo=(mail,subject,bo)=>{
       Email.send({
         Host: "smtp.elasticemail.com",
         Username: "ang311806@gmail.com",
