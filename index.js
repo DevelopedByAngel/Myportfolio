2ceab3a518748459d47ca64971e29e39e6a31a8f
developedbyangel <angelfrancis1111@gmail.com>
2023-09-03 18:20:17 +0530

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ const submit=async(name,mail,contact,message)=>
         To: "angelfrancis1111@gmail.com",
         From: "ang311806@gmail.com",
         Subject: "Angel Francis | Portfolio",
-        Body: name+"<br>"+mail+"<br>"+message+"<br>"+,
+        Body: name+"<br>"+mail+"<br>"+message+"<br>"+cont,
       })
         .then(function (message) {
         	console.log(message);
